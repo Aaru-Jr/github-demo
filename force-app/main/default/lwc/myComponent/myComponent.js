7c6ddbf4b6482e96607afab797755cf9edf43e96
Aravindh A <aarumugam@logitech.com>
2024-02-28 10:48:37 +0530

    modified LWC

--- force-app/main/default/lwc/myComponent/myComponent.js
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class MyComponent extends LightningElement {}
+export default class MyComponent extends LightningElement {
+    @track message = 'Hello, World!';
+
+    get greeting() {
+        return `Hello, ${this.message}!`;
+    }
+}
